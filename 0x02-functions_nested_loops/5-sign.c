89387f7285e43393dce8b3657ac240fa0fa55eac
Imane Ouahmane <imane.ouahmane22@gmail.com>
2023-07-18 18:55:48 -0700

    SIGH

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,16 +7,21 @@
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
-int print_sign(int n) {
-	if ( n > 0 ) {
-		_putchar('+');
-	       return(1);
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
 	}
-		else if ( n == 0 ) {
-			_putchar(0);
-			return(0);
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
 	}
-		else if ( n < 0 ) {
-			_putchar('-');
-			return(-1);		}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
 }
