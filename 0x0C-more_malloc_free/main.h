9451ae7579dbf5328041f245efd01c719a45f00d
Imane Ouahmane <imane.ouahmane22@gmail.com>
2023-08-10 16:51:10 -0700

    REALLOC

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -15,5 +15,5 @@ void errors(void);
 int _strlen(char *s);
 int is_digit(char *s);
 int *array_range(int min, int max);
-
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 #endif
